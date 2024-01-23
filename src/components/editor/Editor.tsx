f1dcf5cd59c5c6626cf9a5dcb458b258e4fe40ea
NivekAle <aexkart@gmail.com>
2024-01-23 00:22:20 -0300

    Editor de texto Lexical instalado no projeto

---
+++ src/components/editor/Editor.tsx
@@ -0,0 +1,60 @@
+import { LexicalComposer } from '@lexical/react/LexicalComposer';
+import { RichTextPlugin } from '@lexical/react/LexicalRichTextPlugin';
+import { ContentEditable } from '@lexical/react/LexicalContentEditable';
+import LexicalErrorBoundary from '@lexical/react/LexicalErrorBoundary';
+
+
+type LexicalEditorProps = {
+	config: Parameters<typeof LexicalComposer>['0']['initialConfig'];
+};
+
+export function LexicalEditor(props: LexicalEditorProps) {
+	return (
+		<LexicalComposer initialConfig={props.config}>
+			<RichTextPlugin
+				contentEditable={<ContentEditable />}
+				placeholder={<Placeholder />}
+				ErrorBoundary={LexicalErrorBoundary}
+			/>
+		</LexicalComposer>
+	);
+}
+
+const Placeholder = () => {
+	return (
+		<div className="absolute top-[1.125rem] left-[1.125rem] opacity-50">
+			Start writing...
+		</div>
+	);
+};
+
+export function Editor() {
+	return (
+		<div
+			id="editor-wrapper"
+			className={
+				'relative prose prose-slate prose-p:my-0 prose-headings:mb-4 prose-headings:mt-2'
+			}
+		>
+			<LexicalEditor
+				config={{
+					namespace: 'lexical-editor',
+					theme: {
+						root: 'p-4 rounded h-full min-h-[200px] bg-slate-50 focus:outline-none focus-visible:border-slate-300',
+						link: 'cursor-pointer',
+						text: {
+							bold: 'font-semibold',
+							underline: 'underline',
+							italic: 'italic',
+							strikethrough: 'line-through',
+							underlineStrikethrough: 'underlined-line-through',
+						},
+					},
+					onError: error => {
+						console.log(error);
+					},
+				}}
+			/>
+		</div>
+	);
+}
